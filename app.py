8c03e78516c864b3572f556e3ded3c6c9930bd16
Jjona26 <jonael.garcia@techexchange.in>
2025-03-09 17:33:08 +0000

    Implement Display_post and tests, Use of display_post on app.py

--- app.py
+++ app.py
@@ -14,11 +14,29 @@ userId = 'user1'
 
 def display_app_page():
     """Displays the home page of the app."""
-    st.title('Welcome to SDS!')
+    #st.title('Welcome to SDS!')
 
     # An example of displaying a custom component called "my_custom_component"
-    value = st.text_input('Enter your name')
-    display_my_custom_component(value)
+    # value = st.text_input('Enter your name')
+    # display_my_custom_component(value)
+
+    # Fetch user posts
+    user_posts = get_user_posts(userId)
+    user_info = get_user_profile(userId)
+
+    # Display section title
+    st.header("User Posts")
+
+    # Loop through posts and display them
+    for post in user_posts:
+        display_post(
+            username = user_info["username"],
+            user_image = user_info["profile_image"],
+            timestamp = post["timestamp"],
+            content = post["content"],
+            post_image = "https://fastly.picsum.photos/id/74/4288/2848.jpg?hmac=q02MzzHG23nkhJYRXR-_RgKTr6fpfwRgcXgE0EKvNB8",
+        )
+        st.markdown("---")  # Adds a separator between posts
 
 
 # This is the starting point for your app. You do not need to change these lines
